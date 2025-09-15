f847785f1567edc6109e3a8185a77f6ab5157af1
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2025-09-15 14:41:39 -0500

    organización de proyecto

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
 
 @app.route("/main")
 def main():
-    return render_template("index.html")
+    return render_template("views/dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
