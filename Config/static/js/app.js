8c9a37647b2271e4d0b066c0a0b097f2e020413f
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2025-09-22 15:54:04 -0500

    se agrega js

--- Config/static/js/app.js
+++ Config/static/js/app.js
@@ -1,8 +1,26 @@
 function saludar() {
-    let nombre = document.getElementById("nombre")
-    let mensaje = nombre.value + " Hola mundo!";
-    alertify.alert('Alert Title', mensaje, function(){ 
-        alertify.success('Ok'); 
-    });
+  let nombre = document.getElementById("nombre");
+  let mensaje = nombre.value + " Hola mundo!";
+  alertify.alert("Alert Title", mensaje, function () {
+    alertify.success("Ok");
+  });
+}
 
+function cargartabla() {
+  axios
+    .get("/cargarTabla", {
+      params: {
+        ID: 12345,
+      },
+    })
+    .then(function (response) {
+      alert(response['data'][0]['name']);
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 }
--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def tablas():
     ]
     return render_template("views/tables.html", empleados=empleados)
 
+@app.route("/cargarTabla")
+def cargarTabla():
+    empleados = [
+        {"name": "Tiger Nixon", "position": "System Architect", "office": "Edinburgh", "age": 61, "start_date": "2011/04/25", "salary": "$320,800"},
+        {"name": "Garrett Winters", "position": "Accountant", "office": "Tokyo", "age": 63, "start_date": "2011/07/25", "salary": "$170,750"},
+        {"name": "Ashton Cox", "position": "Junior Technical Author", "office": "San Francisco", "age": 66, "start_date": "2009/01/12", "salary": "$86,000"},
+        {"name": "Cedric Kelly", "position": "Senior Javascript Developer", "office": "Edinburgh", "age": 22, "start_date": "2012/03/29", "salary": "$433,060"},
+        {"name": "Airi Satou", "position": "Accountant", "office": "Tokyo", "age": 33, "start_date": "2008/11/28", "salary": "$162,700"},
+        {"name": "Brielle Williamson", "position": "Integration Specialist", "office": "New York", "age": 61, "start_date": "2012/12/02", "salary": "$372,000"},
+        {"name": "Herrod Chandler", "position": "Sales Assistant", "office": "San Francisco", "age": 59, "start_date": "2012/08/06", "salary": "$137,500"}
+    ]
+    return empleados
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
